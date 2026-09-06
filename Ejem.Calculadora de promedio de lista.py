d6038a7042bc6e68fc81cab9cf6c507ceec05a92
kevinllancare123-alt <kevinllancare123@gmail.com>
2026-09-06 01:18:58 -0500

    Tercer ejercicio: Calculadora de Promedio con Lista

---
+++ Ejem.Calculadora de promedio de lista.py
@@ -0,0 +1,19 @@
+def calcular_promedio(notas):
+    promedio = sum(notas) / len(notas)
+    nota_min = min(notas)
+    nota_max = max(notas)
+    return promedio, nota_min, nota_max
+
+
+def mostrar_resultado(nombre, notas):
+    prom, mn, mx = calcular_promedio(notas)
+    print("Reporte de:", nombre)
+    print("Notas:", notas)
+    print("Promedio:", prom)
+    print("Nota mínima:", mn)
+    print("Nota máxima:", mx)
+
+
+# Probamos la función
+notas_alumno = [15, 12, 18, 10, 16]
+mostrar_resultado("Kevin", notas_alumno)
